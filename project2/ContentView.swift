a5d07fa4b22f838fb39993a63885dc8b4735f26e
irukahaoruka <erika03301109@gmail.com>
2024-09-14 16:30:56 +0900

    ok

--- project2/ContentView.swift
+++ project2/ContentView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore: Bool = false
     @State private var correctAnswer = Int.random(in: 0...2)
-    private var scoreTitle: String = ""
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State private var scoreTitle: String = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
     
     func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "正解\(countries[correctAnswer])"
+        } else {
+            scoreTitle = "間違ってる！正解は\(countries[correctAnswer])"
+        }
         
     }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
     var body: some View {
-        VStack {
+        HStack {
             ForEach(0..<3) { number in
                 Button {
                     // flag was tapped
+                    flagTapped(number)
+                    askQuestion()
                 } label: {
                     Image(countries[number])
                 }
